73953bf0bf7a5312792fd82e6e52b15c04b82ec0
sirgalas <hudos@rambler.ru>
2018-04-15 17:32:55 +0300

    deploy in heroku

--- config/web.php
+++ config/web.php
@@ -45,6 +45,8 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ],
         ],
         
