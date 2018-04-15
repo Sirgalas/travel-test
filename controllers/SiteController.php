65462bbea03a71b5b0f2f45766774cd27b19fef3
sirgalas <hudos@rambler.ru>
2018-04-15 18:16:16 +0300

    deploy in heroku

---
+++ controllers/SiteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\entities\User;
+use app\search\UserSearch;
+
+class SiteController extends Controller
+{
+
+   
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $searchModel = new UserSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+}
