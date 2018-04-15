025a5556db2d656632e1dcc43d7968790665aae7
sirgalas <hudos@rambler.ru>
2018-04-15 17:17:30 +0300

    deploy in heroku

--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
 
-if ($_SERVER['SERVER_NAME'] == "yiitest.herokuapp.com") {
+//if ($_SERVER['SERVER_NAME'] == "yiitest.herokuapp.com") {
     $host = 'ec2-54-247-81-88.eu-west-1.compute.amazonaws.com';
     $username = 'mowiefedjjfoju';
     $password = 'd7c9bcd85b6ef99dbed84dbab9446560a0c280000f5f61d3fec075adcac52166';
     $dbname = 'd1scssnm5v561k';
     $nameHost = 'pgsql';
-}else{
+/*}else{
     $host = 'localhost;';
     $username = 'root';
     $password = '1';
@@ -21,7 +21,7 @@ var_dump([
     'charset' => 'utf8',
     'enableSchemaCache' => true,
 
-]);
+]);*/
 return [
     'class' => 'yii\db\Connection',
     'dsn' => $nameHost.':host='.$host.' dbname='.$dbname,
