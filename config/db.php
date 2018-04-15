a724a0c2da1247b01566da61aea83f870f34a6e0
sirgalas <hudos@rambler.ru>
2018-04-15 15:19:00 +0300

    db in heroku

--- config/db.php
+++ config/db.php
@@ -1,14 +1,32 @@
 <?php
 
+
+if ($_SERVER['SERVER_NAME'] == "yii2promo.herokuapp.com") {
+    $host = 'ec2-54-247-81-88.eu-west-1.compute.amazonaws.com';
+    $username = 'mowiefedjjfoju';
+    $password = 'd7c9bcd85b6ef99dbed84dbab9446560a0c280000f5f61d3fec075adcac52166';
+    $dbname = 'd1scssnm5v561k';
+    $nameHost = 'pgsql';
+}else{
+    $host = 'localhost;';
+    $username = 'root';
+    $password = '1';
+    $dbname = 'test';
+    $nameHost = 'mysql';
+}
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test',
-    'username' => 'root',
-    'password' => '1',
+    'dsn' => $nameHost.':host='.$host.' dbname='.$dbname,
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
-
+    'enableSchemaCache' => true,
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+
+
+
+
