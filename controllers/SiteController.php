55c0038159a974ee3ca6f8bcb897281e57c82018
sirgalas <hudos@rambler.ru>
2018-04-15 18:21:42 +0300

    deploy in heroku

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -12,21 +12,7 @@ use app\search\UserSearch;
 class SiteController extends Controller
 {
 
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
+   
     /**
      * @inheritdoc
      */
